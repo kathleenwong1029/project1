c271d80347d14ad9257ca9a3d887dacdeffffade
Kathleen Wong <kwong17@stuy.edu>
2018-11-20 11:45:05 -0500

    somethings off

---
+++ shell.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char * readline(){
+  char * str = malloc(100);
+  //printf("$");
+  scanf("%s",str);
+
+  return str;
+}
+
+char ** run(char * line){
+    char * arg = malloc(sizeof(line));
+    strcpy(arg,line);
+    char ** args = calloc(10, sizeof(line));
+    int i = 0;
+    while(i<10){
+      printf("%s\n",args[i]);
+      i++;
+    }
+    return args;
+    //return 0;
+}
+
+int main(){
+  char ** args = run(readline());
+
+  int a = fork();
+  if(!a){
+    execvp(args[0],args);
+  }
+  else{
+
+  }
+  return 0;
+}
