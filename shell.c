3fe1e98a9184692ed55d57365c9452935fdfbc67
Kathleen Wong <kwong17@stuy.edu>
2018-12-01 14:56:28 -0500

    why no worky

--- shell.c
+++ shell.c
@@ -10,17 +10,15 @@ char * readline(){
   return str;
 }
 
-char ** run(char * line){
+char ** parse_args(char * line){
   //printf("%s\n", line);
     char * arg = malloc(sizeof(char *));
     //strcpy(arg , line);
-
     char ** args = calloc(10, sizeof(char*));
     int i = 0;
     while(line){
       arg = strsep(&line, " ");
       args[i]= arg;
-
       //printf("%s\n",args[i]);
       i++;
     }
@@ -31,15 +29,7 @@ void change_d ( char * dir){
     chdir(dir);
 }
 
-
-int main(){
-  int i =1;
-  while(i){
-  char * cdir = malloc(100);
-  printf("%s:$",getcwd(cdir,100));
-
-  char ** args = run(readline());
-
+void run (char ** args){
   if(strcmp(args[0], "exit")==0){
     exit(0);
   }
@@ -56,6 +46,44 @@ int main(){
     int status;
     wait(&status);
   }
+}
 
+
+int main(){
+  while(1){
+  char * cdir = malloc(100);
+  printf("%s:$",getcwd(cdir,100));
+
+  char ** args = parse_args(readline());
+   int i,j, semi= 0;
+
+   char ** c1 = (char **)calloc(10, sizeof(char*));
+   char ** c2 = (char **)calloc(10, sizeof(char*));
+
+   while(args[i]){
+    if(strstr(args[i],";")){
+      semi +=1;
+      j = i;
+      printf("%d\n",j);
+    }
+    i++;
+  }
+  int copy ;
+  for(copy = 0 ; copy < j ;copy ++){
+    printf("%s\n", args[copy]);
+    strcpy(c1[copy],args[copy]);
+    printf("%s\n",c1[copy]);
+  }
+  // for(copy = j + 1 ; copy < sizeof(args) ;copy ++){
+  //   strcpy(c2[copy],args[copy]);
+  // }
+  // if(semi){
+  //   run(c1);
+  //   run(c2);
+  // }
+  // else{
+  //   run(args);
+  // }
+  run(args);
 }
 }
